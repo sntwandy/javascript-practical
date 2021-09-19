54c9339ccc62fe0bca54162230fa9b10b341b93b
Wandy <sntwandy@gmail.com>
2021-09-19 10:27:44 -0400

    Improvement the code with a function and agrupations

--- src/figures/main.js
+++ src/figures/main.js
@@ -1,17 +1,28 @@
 /**
+ *
+ * @param { Text message } message
+ * @param { Number value } value
+ * @param { Value type text } valueType
+ */
+
+const print = (message, value, valueType = "") => {
+	console.log(`${message}: ${value}${valueType}`);
+};
+
+/**
  * Square code
  */
 
 console.group("Square");
 
 const squareSide = 5;
-console.log("The square side is: " + squareSide + "cm");
 
 const squarePerimeter = squareSide * 4;
-console.log("The perimeter of the square is: " + squarePerimeter + "cm");
-
 const squareArea = squareSide * squareSide;
-console.log("The square area is: " + squareArea + "cm'2");
+
+print("Square side", squareSide, "cm");
+print("Square perimeter", squarePerimeter, "cm");
+print("Square area", squareArea, "cm'2");
 
 console.groupEnd();
 
@@ -26,16 +37,16 @@ const triangleSideTwo = 6;
 const baseTriangle = 4;
 const heightTriangle = 5.5;
 
-console.log(
-	`The triangle sides are: ${triangleSideOne}cm, ${triangleSideTwo}cm and ${baseTriangle}cm`
-);
-console.log(`The triangle height: ${heightTriangle}cm`);
-
 const trianglePerimeter = triangleSideOne + triangleSideTwo + baseTriangle;
-console.log(`The perimeter of the triangle is: ${trianglePerimeter}cm`);
-
 const areaTriangle = (baseTriangle * heightTriangle) / 2;
-console.log(`The area of the triangle is: ${areaTriangle}cm'2`);
+
+print("The triangle side one is", triangleSideOne, "cm");
+print("The triangle side two is", triangleSideTwo, "cm");
+print("The base of the triangle is", baseTriangle, "cm");
+
+print("The height of the triangle is", heightTriangle, "cm");
+print("The perimeter of the triangle is", trianglePerimeter, "cm");
+print("The area of the triangle is", areaTriangle, "cm'2");
 
 console.groupEnd();
 
@@ -46,18 +57,15 @@ console.groupEnd();
 console.group("Circle");
 
 const PI = Math.PI;
-console.log(`PI is: ${PI}`);
-
 const radioCircle = 4;
-console.log(`The circle radio is: ${radioCircle}cm`);
-
 const diameterCircle = radioCircle * 2;
-console.log(`The diameter of the circle is: ${diameterCircle}cm`);
-
 const perimeterCircle = Math.round(PI * diameterCircle);
-console.log(`The perimeter of the circle is: ${perimeterCircle}cm`);
-
 const areaCircle = Math.round(PI * (radioCircle * radioCircle));
-console.log(`The area of the circle is: ${areaCircle}cm'2`);
+
+print("The value of PI", PI);
+print("The radio of the circle is", radioCircle, "cm");
+print("The diameter of the circle is", diameterCircle, "cm");
+print("The perimeter of the circle is", perimeterCircle, "cm");
+print("The area of the circle is", areaCircle, "cm'2");
 
 console.groupEnd();
