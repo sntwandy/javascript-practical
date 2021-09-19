f98ab7e5ea1c5b84e2f31d0711352d3d438386b2
Wandy <sntwandy@gmail.com>
2021-09-19 09:24:03 -0400

    Square code, find perimeter and area

--- src/figures/main.js
+++ src/figures/main.js
@@ -1,1 +1,16 @@
-console.log("I want get into the Platzi Team");
+/**
+ * Square code
+ */
+
+const squareSide = 5;
+console.log("The square side is: " + squareSide + "cm");
+
+const squarePerimeter = squareSide * 4;
+console.log("The perimeter of the square is: " + squarePerimeter + "cm");
+
+const squareArea = squareSide * squareSide;
+console.log("The square area is: " + squareArea + "cm'2");
+
+/**
+ * Triangle code
+ */
