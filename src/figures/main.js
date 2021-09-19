1ca4c9cd527a0f8f03ad2daa061d5dfaae0412ee
Wandy <sntwandy@gmail.com>
2021-09-19 09:47:00 -0400

    Triangle, area and perimeter

--- src/figures/main.js
+++ src/figures/main.js
@@ -2,6 +2,8 @@
  * Square code
  */
 
+console.group("Square");
+
 const squareSide = 5;
 console.log("The square side is: " + squareSide + "cm");
 
@@ -11,6 +13,28 @@ console.log("The perimeter of the square is: " + squarePerimeter + "cm");
 const squareArea = squareSide * squareSide;
 console.log("The square area is: " + squareArea + "cm'2");
 
+console.groupEnd();
+
 /**
  * Triangle code
  */
+
+console.group("Triangle");
+
+const triangleSideOne = 6;
+const triangleSideTwo = 6;
+const baseTriangle = 4;
+const heightTriangle = 5.5;
+
+console.log(
+	`The triangle sides are: ${triangleSideOne}cm, ${triangleSideTwo}cm and ${baseTriangle}cm`
+);
+console.log(`The triangle height: ${heightTriangle}cm`);
+
+const trianglePerimeter = triangleSideOne + triangleSideTwo + baseTriangle;
+console.log(`The perimeter of the triangle is: ${trianglePerimeter}cm`);
+
+const areaTriangle = (baseTriangle * heightTriangle) / 2;
+console.log(`The area of the triangle is: ${areaTriangle}cm'2`);
+
+console.groupEnd();
